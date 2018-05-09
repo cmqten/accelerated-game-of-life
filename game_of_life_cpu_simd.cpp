6f23b86de4ad98cc1eb2a8b63f3b546f5aab1668
Carl Marquez <carl.louis.marquez@gmail.com>
2018-05-08 20:18:45 -0400

    fully implemented vector 16 version

--- game_of_life_cpu_simd.cpp
+++ game_of_life_cpu_simd.cpp
@@ -30,7 +30,6 @@ static void game_of_life_cpu_vec16(char* board, int width, int height, int gens)
 
     if (width == 16) {
         for (int i = 0; i < gens; ++i) {
-            // Traverse every cell in row major order for every generation
             for (int y = 0; y < height; ++y) {
                 int irow = y * width;
                 int inorth = (y ? y - 1 : height - 1) * width;
@@ -63,6 +62,111 @@ static void game_of_life_cpu_vec16(char* board, int width, int height, int gens)
             swap_ptr(char*, board, buf);
         }
     }
+    else {
+        for (int i = 0; i < gens; ++i) {
+            for (int y = 0; y < height; ++y) {
+                int irow = y * width;
+                int inorth = (y ? y - 1 : height - 1) * width;
+                int isouth = ((y == (height - 1)) ? 0 : (y + 1)) * width;
+            
+                /* First vector of every row is a special case because the west
+                neighbors wrap around. */
+                __m128i cells;
+                __m128i n_cells = _mm_loadu_si128((__m128i*)(board + inorth));
+                cells = n_cells;
+                __m128i ne_cells = _mm_loadu_si128((__m128i*)(board+inorth+1));
+                cells = _mm_add_epi8(cells, ne_cells);
+
+                /* The elements are shifted one to the east and the first index
+                is replaced by the last cell in the row in order to align the
+                west neighbors with the cell vector. A left shift is done 
+                because the system is little endian. */
+                __m128i nw_cells = _mm_slli_si128(n_cells, 1);
+                ((char*)&nw_cells)[0] = board[inorth + width - 1];
+                cells = _mm_add_epi8(cells, nw_cells);
+
+                __m128i r_cells = _mm_loadu_si128((__m128i*)(board + irow));
+                __m128i e_cells = _mm_loadu_si128((__m128i*)(board + irow + 1));
+                cells = _mm_add_epi8(cells, e_cells);
+                __m128i w_cells = _mm_slli_si128(r_cells, 1);
+                ((char*)&w_cells)[0] = board[irow + width - 1];
+                cells = _mm_add_epi8(cells, w_cells);
+
+                __m128i s_cells = _mm_loadu_si128((__m128i*)(board + isouth));
+                cells = _mm_add_epi8(cells, s_cells);
+                __m128i se_cells = _mm_loadu_si128((__m128i*)(board+isouth+1));
+                cells = _mm_add_epi8(cells, se_cells);
+                __m128i sw_cells = _mm_slli_si128(s_cells, 1);
+                ((char*)&sw_cells)[0] = board[isouth + width - 1];
+                cells = _mm_add_epi8(cells, sw_cells);
+
+                cells = alive_vec16(cells, r_cells);
+                _mm_storeu_si128((__m128i*)(buf + irow), cells);
+
+                /* Middle vectors simple perform unaligned memory accesses to
+                get their east and west neighbors */
+                for (int x = 16; x < width - 16; x += 16) {
+                    /* Initialize cells to the north neighbors to clear the
+                    cells from the previous iteration. */
+                    cells = _mm_loadu_si128((__m128i*)(board + inorth + x));
+                    nw_cells = _mm_loadu_si128((__m128i*)(board+inorth+x-1));
+                    cells = _mm_add_epi8(cells, nw_cells);
+                    ne_cells = _mm_loadu_si128((__m128i*)(board+inorth+x+1));
+                    cells = _mm_add_epi8(cells, ne_cells);
+                    
+                    w_cells = _mm_loadu_si128((__m128i*)(board + irow + x - 1));
+                    cells = _mm_add_epi8(cells, w_cells);
+                    e_cells = _mm_loadu_si128((__m128i*)(board + irow + x + 1));
+                    cells = _mm_add_epi8(cells, e_cells);
+
+                    s_cells = _mm_loadu_si128((__m128i*)(board + isouth + x));
+                    cells = _mm_add_epi8(cells, s_cells);
+                    sw_cells = _mm_loadu_si128((__m128i*)(board+isouth+x-1));
+                    cells = _mm_add_epi8(cells, sw_cells);
+                    se_cells = _mm_loadu_si128((__m128i*)(board+isouth+x+1));
+                    cells = _mm_add_epi8(cells, se_cells);
+
+                    r_cells = _mm_loadu_si128((__m128i*)(board + irow + x));
+                    cells = alive_vec16(cells, r_cells);
+                    _mm_storeu_si128((__m128i*)(buf + irow + x), cells);
+                }
+
+                /* Last vector of every row is a special case because the east
+                neighbors wrap around */
+                n_cells = _mm_loadu_si128((__m128i*)(board+inorth+width-16));
+                cells = n_cells;
+                nw_cells = _mm_loadu_si128((__m128i*)(board+inorth+width-17));
+                cells = _mm_add_epi8(cells, nw_cells);
+
+                /* The elements are shifted one to the west and the last index
+                is replaced by the first cell in the row in order to align the
+                east neighbors with the cell vector. A right shift is done 
+                because the system is little endian. */
+                ne_cells = _mm_srli_si128(n_cells, 1);
+                ((char*)&ne_cells)[15] = board[inorth];
+                cells = _mm_add_epi8(cells, ne_cells);
+                
+                r_cells = _mm_loadu_si128((__m128i*)(board+irow+width-16));
+                w_cells = _mm_loadu_si128((__m128i*)(board+irow+width-17));
+                cells = _mm_add_epi8(cells, w_cells);
+                e_cells = _mm_srli_si128(r_cells, 1);
+                ((char*)&e_cells)[15] = board[irow];
+                cells = _mm_add_epi8(cells, e_cells);
+
+                s_cells = _mm_loadu_si128((__m128i*)(board+isouth+width-16));
+                cells = _mm_add_epi8(cells, s_cells);
+                sw_cells = _mm_loadu_si128((__m128i*)(board+isouth+width-17));
+                cells = _mm_add_epi8(cells, sw_cells);
+                se_cells = _mm_srli_si128(s_cells, 1);
+                ((char*)&se_cells)[15] = board[isouth];
+                cells = _mm_add_epi8(cells, se_cells);
+
+                cells = alive_vec16(cells, r_cells);
+                _mm_storeu_si128((__m128i*)(buf + irow + width - 16), cells);
+            }
+            swap_ptr(char*, board, buf);
+        }
+    }
 
     /**
      * Always want both board and buf to be pointing at their original addresses
@@ -81,7 +185,5 @@ static void game_of_life_cpu_vec16(char* board, int width, int height, int gens)
  */ 
 void game_of_life_cpu_simd(char* board, int width, int height, int gens)
 {
-    if (width == 16) {
-        game_of_life_cpu_vec16(board, width, height, gens);
-    }
+    game_of_life_cpu_vec16(board, width, height, gens);
 }
