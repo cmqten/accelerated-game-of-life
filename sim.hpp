f42f0506a42de01c7691cd444219877ffc843ed7
Carl Marquez <carl.louis.marquez@gmail.com>
2018-05-20 02:26:26 -0400

    Restructued sim_cpu_simd, finished sim_cpu_omp - Moved inlined functions from sim_cpu_simd.cpp to sim_cpu_simd.hpp so they can be referenced from a different translation unit - Made inline functions in sim_cpu_simd.hpp static - Implemented sim_cpu_omp

--- sim.hpp
+++ sim.hpp
@@ -11,14 +11,19 @@
 #ifndef __SIM_HPP__
 #define __SIM_HPP__
 
+/* Optimized CPU sequential implementation */
 void sim_cpu_sequential(char* grid, int width, int height, int gens);
 
+/* Optimized single thread CPU SIMD implementation */ 
 void sim_cpu_simd(char* grid, int width, int height, int gens);
 
+/* Optimized multithread CPU SIMD implementation using OpenMP */
 void sim_cpu_threads(char* grid, int width, int height, int gens);
 
+/* Optimized multithread CPU SIMD implementation using std::thread */
 void sim_cpu_omp(char* grid, int width, int height, int gens);
 
+/* Optimized GPU SIMD implementation using OpenCL */
 void sim_gpu_opencl(char* grid, int width, int height, int gens);
 
 #endif
