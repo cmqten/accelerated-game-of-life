ef0e04cfa2076878953a4034f6fe5ca68884a333
Carl Marquez <carl.louis.marquez@gmail.com>
2018-08-02 00:31:52 -0400

    Updated build script and CMakeLists.txt
    - build.sh now asks for C++ compiler and OpenCL location
    - CMakeLists.txt gets OpenCL location from environment variable

--- build.sh
+++ build.sh
@@ -2,27 +2,45 @@
 
 PROJDIR=`dirname $(realpath $0)`
 OUTDIR="$PROJDIR/build"
+NUMPROCS=`cat /proc/cpuinfo | grep "^processor" | wc -l`
 
 function usage_msg {
     echo "Usage:"
-    echo "    build.sh : build project"
-    echo "    build.sh clean : clean project"
+    echo "    build.sh [options]"
+    echo ""
+    echo "Options:"
+    echo "    -c        : clean build tree instead of compiling"
+    echo "    -j N      : use N jobs to compile"
 }
 
-if [ $# == 0 ] && [ -e $OUTDIR ]; then # Build if build directory exists already
+while getopts "cj:" arg; do 
+    case $arg in
+        c)  rm -rf $OUTDIR; exit 0;; # clean
+
+        j)  # number of parallel jobs to spawn during compile
+            if [ ${OPTARG} -eq ${OPTARG} 2> /dev/null ]; then
+                NUMPROCS=${OPTARG}
+            else
+                usage_msg; exit 1;
+            fi;;
+
+        ?)  usage_msg; exit 1;;
+    esac
+done
+
+if [ -e $OUTDIR ]; then # Build if build directory exists already
     cd $OUTDIR
-    make -j 4
-elif [ $# == 0 ] && [ ! -e $OUTDIR ]; then # Create build directory first
+    make -j $NUMPROCS
+elif [ ! -e $OUTDIR ]; then # Create build directory first
+    read -e -p "Enter C++ compiler [default: detected by CMake]: " CXX
+    export CXX
+    read -e -p "Enter OpenCL directory [default: none]: " OCLDIR
+    export OCLDIR
+
     mkdir $OUTDIR
     cd $OUTDIR
     cmake ../
-    make -j 4
-elif [ $# == 1 ]; then 
-    if [ $1 == "clean" ]; then # Delete build directory
-        rm -rf $OUTDIR
-    else
-        usage_msg
-    fi    
+    make -j $NUMPROCS
 else
     usage_msg
 fi
