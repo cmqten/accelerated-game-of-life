b1a98a8569822147ee6927636e34923a6cdcb353
Carl Marquez <carl.louis.marquez@gmail.com>
2019-12-26 20:02:13 -0500

    cpu simd sse changes
    - added sse4.1 element insertion to vector
    - fallback to integer vector if sse2, ssse3 not available

--- include/cpu_simd.hpp
+++ include/cpu_simd.hpp
@@ -16,151 +16,6 @@
 #include <util.hpp>
 
 /*******************************************************************************
- * CPU SIMD 128-bit vector SSE2/SSSE3
- * 
- * Processes 16 cells simultaneously.
- ******************************************************************************/
-
-void cpu_simd_16(char* grid, int width, int height, int gens);
-
-/* Calculates the next states of 16 cells in a vector. */
-static inline __m128i cpu_simd_16_alive(__m128i cells, __m128i neighbors_count)
-{
-    __m128i has_3_neighbors = _mm_cmpeq_epi8(neighbors_count, _mm_set1_epi8(3));
-    __m128i has_2_neighbors = _mm_cmpeq_epi8(neighbors_count, _mm_set1_epi8(2));
-    __m128i alive_has_2_neighbors = _mm_and_si128(cells, has_2_neighbors);
-    cells = _mm_or_si128(has_3_neighbors, alive_has_2_neighbors);
-    return _mm_and_si128(cells, _mm_set1_epi8(1));
-}
-
-/* Processes rows with exactly 16 width. */
-static inline void cpu_simd_16_row_16w(char* grid, char* buf, int y, int y_north, int y_south)
-{
-    int width = 16;
-    int i_row = y * width;
-    int i_north = y_north * width;
-    int i_south = y_south * width;
-
-    // East/west, northeast/northwest, southeast/southwest cells are rotations
-    // of current cells, north, south cells, respectively.
-    __m128i cells = _mm_load_si128((__m128i*)(grid + i_row));
-    __m128i n_cells = _mm_load_si128((__m128i*)(grid + i_north));
-    __m128i ne_cells = _mm_alignr_epi8(n_cells, n_cells, 1);
-    __m128i nw_cells = _mm_alignr_epi8(n_cells, n_cells, 15);
-    __m128i e_cells = _mm_alignr_epi8(cells, cells, 1);
-    __m128i w_cells = _mm_alignr_epi8(cells, cells, 15);
-    __m128i s_cells = _mm_load_si128((__m128i*)(grid + i_south));
-    __m128i se_cells = _mm_alignr_epi8(s_cells, s_cells, 1);
-    __m128i sw_cells = _mm_alignr_epi8(s_cells, s_cells, 15);
-
-    __m128i neighbors_count = n_cells;
-    neighbors_count = _mm_add_epi8(neighbors_count, ne_cells);
-    neighbors_count = _mm_add_epi8(neighbors_count, nw_cells);
-    neighbors_count = _mm_add_epi8(neighbors_count, e_cells);
-    neighbors_count = _mm_add_epi8(neighbors_count, w_cells);
-    neighbors_count = _mm_add_epi8(neighbors_count, s_cells);
-    neighbors_count = _mm_add_epi8(neighbors_count, se_cells);
-    neighbors_count = _mm_add_epi8(neighbors_count, sw_cells);
-
-    cells = cpu_simd_16_alive(cells, neighbors_count);
-    _mm_store_si128((__m128i*)(buf + i_row), cells);
-}
-
-/* Processes a row with greater than 16 width. */
-static inline void cpu_simd_16_row(char* grid, char* buf, int width, int y, int y_north, int y_south)
-{
-    int i_row = y * width;
-    int i_north = y_north * width;
-    int i_south = y_south * width;
-
-    // Pointers to the start of the north, current, and south rows
-    char* p_north = grid + i_north;
-    char* p_row = grid + i_row;
-    char* p_south = grid + i_south;
-
-    // First vector is a special case because the west neighbors wrap around. 
-    // To access the west, northwest, southwest cells, the current cells, north, 
-    // south cells, repectively, are left shifted one and the first vector 
-    // element is replaced by the last cell in their respective rows.
-    __m128i neighbors_count;
-    __m128i cells = _mm_loadu_si128((__m128i*)(p_row));
-    __m128i n_cells = _mm_loadu_si128((__m128i*)(p_north));
-    __m128i ne_cells = _mm_loadu_si128((__m128i*)(p_north + 1));
-    __m128i nw_cells = _mm_alignr_epi8(n_cells, _mm_set1_epi8(p_north[width - 1]), 15);
-    __m128i e_cells = _mm_loadu_si128((__m128i*)(p_row + 1));
-    __m128i w_cells = _mm_alignr_epi8(cells, _mm_set1_epi8(p_row[width - 1]), 15);
-    __m128i s_cells = _mm_loadu_si128((__m128i*)(p_south));
-    __m128i se_cells = _mm_loadu_si128((__m128i*)(p_south + 1));
-    __m128i sw_cells = _mm_alignr_epi8(s_cells, _mm_set1_epi8(p_south[width - 1]), 15);
-
-    neighbors_count = n_cells;
-    neighbors_count = _mm_add_epi8(neighbors_count, ne_cells);
-    neighbors_count = _mm_add_epi8(neighbors_count, nw_cells);
-    neighbors_count = _mm_add_epi8(neighbors_count, e_cells);
-    neighbors_count = _mm_add_epi8(neighbors_count, w_cells);
-    neighbors_count = _mm_add_epi8(neighbors_count, s_cells);
-    neighbors_count = _mm_add_epi8(neighbors_count, se_cells);
-    neighbors_count = _mm_add_epi8(neighbors_count, sw_cells);
-
-    cells = cpu_simd_16_alive(cells, neighbors_count);
-    _mm_storeu_si128((__m128i*)(buf + i_row), cells);
-
-    // Middle vectors
-    for (int x = 16; x < width - 16; x += 16) {
-        int x_east = x + 1;
-        int x_west = x - 1;
-
-        cells = _mm_loadu_si128((__m128i*)(p_row + x));
-        n_cells = _mm_loadu_si128((__m128i*)(p_north + x));
-        nw_cells = _mm_loadu_si128((__m128i*)(p_north + x_west));
-        ne_cells = _mm_loadu_si128((__m128i*)(p_north + x_east));
-        w_cells = _mm_loadu_si128((__m128i*)(p_row + x_west));
-        e_cells = _mm_loadu_si128((__m128i*)(p_row + x_east));
-        s_cells = _mm_loadu_si128((__m128i*)(p_south + x));
-        sw_cells = _mm_loadu_si128((__m128i*)(p_south + x_west));
-        se_cells = _mm_loadu_si128((__m128i*)(p_south + x_east));
-
-        neighbors_count = n_cells;
-        neighbors_count = _mm_add_epi8(neighbors_count, ne_cells);
-        neighbors_count = _mm_add_epi8(neighbors_count, nw_cells);
-        neighbors_count = _mm_add_epi8(neighbors_count, e_cells);
-        neighbors_count = _mm_add_epi8(neighbors_count, w_cells);
-        neighbors_count = _mm_add_epi8(neighbors_count, s_cells);
-        neighbors_count = _mm_add_epi8(neighbors_count, se_cells);
-        neighbors_count = _mm_add_epi8(neighbors_count, sw_cells);
-
-        cells = cpu_simd_16_alive(cells, neighbors_count);
-        _mm_storeu_si128((__m128i*)(buf + i_row + x), cells);
-    }
-
-    // Last vector is a special case because the east neighbors wrap around. 
-    // To access the east, northeast, southeast cells, the current cells, north, 
-    // south cells, repectively, are right shifted one and the last vector 
-    // element is replaced by the first cell in their respective rows.
-    cells = _mm_loadu_si128((__m128i*)(p_row + width - 16));
-    n_cells = _mm_loadu_si128((__m128i*)(p_north + width - 16));
-    nw_cells = _mm_loadu_si128((__m128i*)(p_north + width - 17));
-    ne_cells = _mm_alignr_epi8(_mm_set1_epi8(*p_north), n_cells, 1);
-    w_cells = _mm_loadu_si128((__m128i*)(p_row + width - 17));
-    e_cells = _mm_alignr_epi8(_mm_set1_epi8(*p_row), cells, 1);
-    s_cells = _mm_loadu_si128((__m128i*)(p_south + width - 16));
-    sw_cells = _mm_loadu_si128((__m128i*)(p_south + width - 17));
-    se_cells = _mm_alignr_epi8(_mm_set1_epi8(*p_south), s_cells, 1);
-
-    neighbors_count = n_cells;
-    neighbors_count = _mm_add_epi8(neighbors_count, nw_cells);
-    neighbors_count = _mm_add_epi8(neighbors_count, ne_cells);
-    neighbors_count = _mm_add_epi8(neighbors_count, w_cells);
-    neighbors_count = _mm_add_epi8(neighbors_count, e_cells);
-    neighbors_count = _mm_add_epi8(neighbors_count, s_cells);
-    neighbors_count = _mm_add_epi8(neighbors_count, sw_cells);
-    neighbors_count = _mm_add_epi8(neighbors_count, se_cells);
-
-    cells = cpu_simd_16_alive(cells, neighbors_count);
-    _mm_storeu_si128((__m128i*)(buf + i_row + width - 16), cells);
-}
-
-/*******************************************************************************
  * CPU SIMD integer type vector
  * 
  * Processes n cells simultaneously, where n is the size of the integer type 
@@ -327,6 +182,169 @@ void cpu_simd_int(char* grid, int width, int height, int gens)
         memcpy(grid, buf, size);
     }
     delete[] buf;
+}
+
+/*******************************************************************************
+ * CPU SIMD 128-bit vector SSE2/SSSE3
+ * 
+ * Processes 16 cells simultaneously.
+ ******************************************************************************/
+
+#ifdef __SSE4_1__
+#define shift_in_first_16(vec, val) _mm_insert_epi8(_mm_slli_si128(vec, 1), val, 0)
+#define shift_in_last_16(vec, val) _mm_insert_epi8(_mm_srli_si128(vec, 1), val, 15)
+#else
+#define shift_in_first_16(vec, val) _mm_alignr_epi8(vec, _mm_set1_epi8(val), 15) 
+#define shift_in_last_16(vec, val) _mm_alignr_epi8(_mm_set1_epi8(val), vec, 1)
+#endif
+
+void cpu_simd_16(char* grid, int width, int height, int gens);
+
+#if defined __SSE2__ && defined __SSSE3__
+/* Calculates the next states of 16 cells in a vector. */
+static inline __m128i cpu_simd_16_alive(__m128i cells, __m128i neighbors_count)
+{
+    __m128i has_3_neighbors = _mm_cmpeq_epi8(neighbors_count, _mm_set1_epi8(3));
+    __m128i has_2_neighbors = _mm_cmpeq_epi8(neighbors_count, _mm_set1_epi8(2));
+    __m128i alive_has_2_neighbors = _mm_and_si128(cells, has_2_neighbors);
+    cells = _mm_or_si128(has_3_neighbors, alive_has_2_neighbors);
+    return _mm_and_si128(cells, _mm_set1_epi8(1));
+}
+#endif
+
+/* Processes rows with exactly 16 width. */
+static inline void cpu_simd_16_row_16w(char* grid, char* buf, int y, int y_north, int y_south)
+{
+#if defined __SSE2__ && defined __SSSE3__
+    int width = 16;
+    int i_row = y * width;
+    int i_north = y_north * width;
+    int i_south = y_south * width;
+
+    // East/west, northeast/northwest, southeast/southwest cells are rotations
+    // of current cells, north, south cells, respectively.
+    __m128i cells = _mm_load_si128((__m128i*)(grid + i_row));
+    __m128i n_cells = _mm_load_si128((__m128i*)(grid + i_north));
+    __m128i ne_cells = _mm_alignr_epi8(n_cells, n_cells, 1);
+    __m128i nw_cells = _mm_alignr_epi8(n_cells, n_cells, 15);
+    __m128i e_cells = _mm_alignr_epi8(cells, cells, 1);
+    __m128i w_cells = _mm_alignr_epi8(cells, cells, 15);
+    __m128i s_cells = _mm_load_si128((__m128i*)(grid + i_south));
+    __m128i se_cells = _mm_alignr_epi8(s_cells, s_cells, 1);
+    __m128i sw_cells = _mm_alignr_epi8(s_cells, s_cells, 15);
+
+    __m128i neighbors_count = n_cells;
+    neighbors_count = _mm_add_epi8(neighbors_count, ne_cells);
+    neighbors_count = _mm_add_epi8(neighbors_count, nw_cells);
+    neighbors_count = _mm_add_epi8(neighbors_count, e_cells);
+    neighbors_count = _mm_add_epi8(neighbors_count, w_cells);
+    neighbors_count = _mm_add_epi8(neighbors_count, s_cells);
+    neighbors_count = _mm_add_epi8(neighbors_count, se_cells);
+    neighbors_count = _mm_add_epi8(neighbors_count, sw_cells);
+
+    cells = cpu_simd_16_alive(cells, neighbors_count);
+    _mm_store_si128((__m128i*)(buf + i_row), cells);
+#else
+    cpu_simd_int_row<uint64_t>(grid, buf, 16, y, y_north, y_south);
+#endif
+}
+
+/* Processes a row with greater than 16 width. */
+static inline void cpu_simd_16_row(char* grid, char* buf, int width, int y, int y_north, int y_south)
+{
+#if defined __SSE2__ && defined __SSSE3__
+    int i_row = y * width;
+    int i_north = y_north * width;
+    int i_south = y_south * width;
+
+    // Pointers to the start of the north, current, and south rows
+    char* p_north = grid + i_north;
+    char* p_row = grid + i_row;
+    char* p_south = grid + i_south;
+
+    // First vector is a special case because the west neighbors wrap around. 
+    // To access the west, northwest, southwest cells, the current cells, north, 
+    // south cells, repectively, are left shifted one and the first vector 
+    // element is replaced by the last cell in their respective rows.
+    __m128i neighbors_count;
+    __m128i cells = _mm_loadu_si128((__m128i*)(p_row));
+    __m128i n_cells = _mm_loadu_si128((__m128i*)(p_north));
+    __m128i ne_cells = _mm_loadu_si128((__m128i*)(p_north + 1));
+    __m128i nw_cells = shift_in_first_16(n_cells, p_north[width - 1]);
+    __m128i e_cells = _mm_loadu_si128((__m128i*)(p_row + 1));
+    __m128i w_cells = shift_in_first_16(cells, p_row[width - 1]);
+    __m128i s_cells = _mm_loadu_si128((__m128i*)(p_south));
+    __m128i se_cells = _mm_loadu_si128((__m128i*)(p_south + 1));
+    __m128i sw_cells = shift_in_first_16(s_cells, p_south[width - 1]);
+
+    neighbors_count = n_cells;
+    neighbors_count = _mm_add_epi8(neighbors_count, ne_cells);
+    neighbors_count = _mm_add_epi8(neighbors_count, nw_cells);
+    neighbors_count = _mm_add_epi8(neighbors_count, e_cells);
+    neighbors_count = _mm_add_epi8(neighbors_count, w_cells);
+    neighbors_count = _mm_add_epi8(neighbors_count, s_cells);
+    neighbors_count = _mm_add_epi8(neighbors_count, se_cells);
+    neighbors_count = _mm_add_epi8(neighbors_count, sw_cells);
+
+    cells = cpu_simd_16_alive(cells, neighbors_count);
+    _mm_storeu_si128((__m128i*)(buf + i_row), cells);
+
+    // Middle vectors
+    for (int x = 16; x < width - 16; x += 16) {
+        int x_east = x + 1;
+        int x_west = x - 1;
+
+        cells = _mm_loadu_si128((__m128i*)(p_row + x));
+        n_cells = _mm_loadu_si128((__m128i*)(p_north + x));
+        nw_cells = _mm_loadu_si128((__m128i*)(p_north + x_west));
+        ne_cells = _mm_loadu_si128((__m128i*)(p_north + x_east));
+        w_cells = _mm_loadu_si128((__m128i*)(p_row + x_west));
+        e_cells = _mm_loadu_si128((__m128i*)(p_row + x_east));
+        s_cells = _mm_loadu_si128((__m128i*)(p_south + x));
+        sw_cells = _mm_loadu_si128((__m128i*)(p_south + x_west));
+        se_cells = _mm_loadu_si128((__m128i*)(p_south + x_east));
+
+        neighbors_count = n_cells;
+        neighbors_count = _mm_add_epi8(neighbors_count, ne_cells);
+        neighbors_count = _mm_add_epi8(neighbors_count, nw_cells);
+        neighbors_count = _mm_add_epi8(neighbors_count, e_cells);
+        neighbors_count = _mm_add_epi8(neighbors_count, w_cells);
+        neighbors_count = _mm_add_epi8(neighbors_count, s_cells);
+        neighbors_count = _mm_add_epi8(neighbors_count, se_cells);
+        neighbors_count = _mm_add_epi8(neighbors_count, sw_cells);
+
+        cells = cpu_simd_16_alive(cells, neighbors_count);
+        _mm_storeu_si128((__m128i*)(buf + i_row + x), cells);
+    }
+
+    // Last vector is a special case because the east neighbors wrap around. 
+    // To access the east, northeast, southeast cells, the current cells, north, 
+    // south cells, repectively, are right shifted one and the last vector 
+    // element is replaced by the first cell in their respective rows.
+    cells = _mm_loadu_si128((__m128i*)(p_row + width - 16));
+    n_cells = _mm_loadu_si128((__m128i*)(p_north + width - 16));
+    nw_cells = _mm_loadu_si128((__m128i*)(p_north + width - 17));
+    ne_cells = shift_in_last_16(n_cells, *p_north);
+    w_cells = _mm_loadu_si128((__m128i*)(p_row + width - 17));
+    e_cells = shift_in_last_16(cells, *p_row);
+    s_cells = _mm_loadu_si128((__m128i*)(p_south + width - 16));
+    sw_cells = _mm_loadu_si128((__m128i*)(p_south + width - 17));
+    se_cells = shift_in_last_16(s_cells, *p_south);
+
+    neighbors_count = n_cells;
+    neighbors_count = _mm_add_epi8(neighbors_count, nw_cells);
+    neighbors_count = _mm_add_epi8(neighbors_count, ne_cells);
+    neighbors_count = _mm_add_epi8(neighbors_count, w_cells);
+    neighbors_count = _mm_add_epi8(neighbors_count, e_cells);
+    neighbors_count = _mm_add_epi8(neighbors_count, s_cells);
+    neighbors_count = _mm_add_epi8(neighbors_count, sw_cells);
+    neighbors_count = _mm_add_epi8(neighbors_count, se_cells);
+
+    cells = cpu_simd_16_alive(cells, neighbors_count);
+    _mm_storeu_si128((__m128i*)(buf + i_row + width - 16), cells);
+#else
+    cpu_simd_int_row<uint64_t>(grid, buf, width, y, y_north, y_south);
+#endif
 }
 
 #endif
